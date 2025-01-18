a27e0d3883d56f875378a48283224a95cfb4a7ce
Breno <brenofreitas28f@gmail.com>
2025-01-18 11:24:24 -0300

    Exercicio sobre tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Breno";
+        String address = "Avenida Paulista";
+        String salaryDate = "23/02/2025";
+        double salary = 5000.00;
+
+        String output = "Eu " + name + ", morando no endereço " + address + ", confirmo que recebi o salário de R$" + salary + ", na data " + salaryDate;
+
+        System.out.println(output);
+    }
+}
